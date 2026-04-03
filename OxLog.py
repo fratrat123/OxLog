86cbdf9ec05f55408eaa11a4caa30004198e865a
Modworksgg <159961822+fratrat123@users.noreply.github.com>
2026-04-03 11:27:05 -0400

    Bump OxLog version to 1.1.6

--- OxLog.py
+++ OxLog.py
@@ -26,7 +26,7 @@ app.secret_key = get_secret_key()
 CONFIG_FILE = "config.json"
 LOG_FILE = "plugin_changelog.txt"
 VERSIONS_DIR = "versions"
-OXLOG_VERSION = "1.1.5"
+OXLOG_VERSION = "1.1.6"
 UPDATE_URL = "https://raw.githubusercontent.com/fratrat123/OxLog/main/version.json"
 
 DEFAULT_CONFIG = {
